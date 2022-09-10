2674090b51d124dc6daff08c3f82ab209f585060
basil-huber <basil.huber@gmail.com>
2022-09-10 10:19:14 +0200

    updates redlooper_app

--- buildroot-external/package/python-redlooper-app/python-redlooper-app.mk
+++ buildroot-external/package/python-redlooper-app/python-redlooper-app.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_REDLOOPER_APP_VERSION = v0.1.0
+PYTHON_REDLOOPER_APP_VERSION = v0.1.1
 PYTHON_REDLOOPER_APP_SITE = $(call github,basil-huber,RedlooperApp,$(PYTHON_REDLOOPER_APP_VERSION))
 PYTHON_REDLOOPER_APP_SETUP_TYPE = setuptools
 PYTHON_REDLOOPER_APP_LICENSE = MIT
